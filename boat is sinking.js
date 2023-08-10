697fc1808a26fe09049c389b8c66c9322efc9a49
IbnMikhail <141636782+IbnMikhail@users.noreply.github.com>
2023-08-11 00:31:08 +0100

    initial commit

--- boat is sinking.js
+++ boat is sinking.js
@@ -1,17 +1,9 @@
-function calculateRemainingParticipants(initialParticipants, rounds) {
-    let remainingParticipants = initialParticipants;
+function sinking(participant, rounds) {
+  let remainingParticipants = participant;
+  for (const groupSize of rounds) {
+    const qualifiedParticipants = remainingParticipants - (remainingParticipants % groupSize);
+    remainingParticipants = qualifiedParticipants;
+  }
 
-    for (let i = 0; i < rounds.length; i++) {
-        const groupSize = rounds[i];
-        const participantsLeftInGroup = Math.floor(remainingParticipants * (groupSize / (groupSize + 1)));
-        remainingParticipants = participantsLeftInGroup;
-    }
-
-    return remainingParticipants;
+  return remainingParticipants;
 }
-
-const initialParticipants = 15;
-const rounds = [4, 2, 5, 3, 2];
-const result = calculateRemainingParticipants(initialParticipants, rounds);
-
-console.log("Remaining participants:", result); 
