a42ad3749ce1f97a42f0cd2914436a8fb649be2a
IbnMikhail <141636782+IbnMikhail@users.noreply.github.com>
2023-08-11 00:30:26 +0100

    initial commit

--- Basic Math Problem.js
+++ Basic Math Problem.js
@@ -1,15 +1,14 @@
 function basicmath(word) {
-  
   const expression = word
     .replace("plus", "+")
     .replace("minus", "-")
     .replace("times", "*")
     .replace("divided by", "/");
-
   try {
     const result = eval(expression);
     return result;
   } catch (error) {
-    return null; // Invalid expression
+    return null; 
   }
 }
+
