875b5f61cb8ae3a546b639f688232d882e9eb345
IbnMikhail <141636782+IbnMikhail@users.noreply.github.com>
2023-08-11 00:12:15 +0100

    initial commit

--- Basic Math Problem.js
+++ Basic Math Problem.js
@@ -1,51 +1,15 @@
-function wordToOperator(word) {
-    const operators = {
-        'plus': '+',
-        'minus': '-',
-        'times': '*',
-        'divided by': '/'
-    };
-    return operators[word] || null;
-}
+function basicmath(word) {
+  
+  const expression = word
+    .replace("plus", "+")
+    .replace("minus", "-")
+    .replace("times", "*")
+    .replace("divided by", "/");
 
-function calculateExpression(expression) {
-    const words = expression.split(' ');
-
-    if (words.length % 2 === 0) {
-        return null; 
-    }
-
-    let total = parseFloat(words[0]);
-    for (let i = 1; i < words.length; i += 2) {
-        const operator = wordToOperator(words[i]);
-        if (!operator) {
-            return null; 
-        }
-
-        const operand = parseFloat(words[i + 1]);
-
-        if (operator === '+') {
-            total += operand;
-        } else if (operator === '-') {
-            total -= operand;
-        } else if (operator === '*') {
-            total *= operand;
-        } else if (operator === '/') {
-            if (operand === 0) {
-                return null; 
-            }
-            total /= operand;
-        }
-    }
-
-    return total;
-}
-
-const inputExpression = prompt("Enter the expression in words: ");
-const result = calculateExpression(inputExpression);
-
-if (result === null) {
-    console.log("Invalid input or operator.");
-} else {
-    console.log("Result:", result);
+  try {
+    const result = eval(expression);
+    return result;
+  } catch (error) {
+    return null; // Invalid expression
+  }
 }
