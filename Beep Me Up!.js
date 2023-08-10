f19aa2f2ca3cbca7a8eff3908567183a293c77e6
IbnMikhail <141636782+IbnMikhail@users.noreply.github.com>
2023-08-10 23:59:21 +0100

    Update Beep Me Up!.js

--- Beep Me Up!.js
+++ Beep Me Up!.js
@@ -1,24 +1,24 @@
-function generateMessage(contacts) {
-    // Filter out valid contacts (names with only letters and spaces)
-    const validContacts = [];
-    for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
-        if (/^[A-Za-z\s]+$/.test(contact)) {
-            validContacts.push(contact);
-        }
-    }
+function Messages(con) {
+  const contacts = con;
+  const messagesFrom = [];
 
-    // Count the number of valid contacts
-    const validContactCount = validContacts.length;
-
-    // Generate the message based on the count of valid contacts
-    if (validContactCount === 0) {
+  return {
+    send: function(sender) {
+      messagesFrom.push(sender);
+    },
+    getMessageNotification: function() {
+      const validContacts = contacts.filter(contact => messagesFrom.includes(contact));
+      
+      if (validContacts.length === 0) {
         return "No one sent you a message!";
-    } else if (validContactCount === 1) {
-        return validContacts[0] + " sent you a message!";
-    } else if (validContactCount === 2) {
-        return validContacts[0] + " and " + validContacts[1] + " sent you a message!";
-    } else {
-        return validContacts[0] + ", " + validContacts[1] + " and " + (validContactCount - 2) + " others sent you a message!";
+      } else if (validContacts.length === 1) {
+        return `${validContacts[0]} sent you a message!`;
+      } else if (validContacts.length === 2) {
+        return `${validContacts[0]} and ${validContacts[1]} sent you a message!`;
+      } else {
+        const othersCount = validContacts.length - 2;
+        return `${validContacts[0]}, ${validContacts[1]} and ${othersCount} others sent you a message!`;
+      }
     }
+  };
 }
