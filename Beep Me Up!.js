06b45fcdd9d69efc4bc7d4bb4050ade76a198756
IbnMikhail <141636782+IbnMikhail@users.noreply.github.com>
2023-08-11 00:43:39 +0100

    initial commit

--- Beep Me Up!.js
+++ Beep Me Up!.js
@@ -1,24 +1,22 @@
 function Messages(con) {
-  const contacts = con;
-  const messagesFrom = [];
-
-  return {
-    send: function(sender) {
-      messagesFrom.push(sender);
-    },
-    getMessageNotification: function() {
-      const validContacts = contacts.filter(contact => messagesFrom.includes(contact));
-      
-      if (validContacts.length === 0) {
-        return "No one sent you a message!";
-      } else if (validContacts.length === 1) {
-        return `${validContacts[0]} sent you a message!`;
-      } else if (validContacts.length === 2) {
-        return `${validContacts[0]} and ${validContacts[1]} sent you a message!`;
-      } else {
-        const othersCount = validContacts.length - 2;
-        return `${validContacts[0]}, ${validContacts[1]} and ${othersCount} others sent you a message!`;
-      }
+  const validContacts = [];
+  
+  for (const contact of con) {
+    if (!contact.includes(" ")) {
+      validContacts.push(contact);
     }
-  };
+  }
+
+  const count = validContacts.length;
+
+  if (count === 0) {
+    return "No one sent you a message!";
+  } else if (count === 1) {
+    return `${validContacts[0]} sent you a message!`;
+  } else if (count === 2) {
+    return `${validContacts[0]} and ${validContacts[1]} sent you a message!`;
+  } else {
+    const othersCount = count - 2;
+    return `${validContacts[0]}, ${validContacts[1]} and ${othersCount} others sent you a message!`;
+  }
 }
